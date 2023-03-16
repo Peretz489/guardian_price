54f54f06647fc4c7b83d78f6b7bcb708d459d07a
Peretz489 <peretz489@gmail.com>
2023-03-16 22:34:13 +0300

    in work

--- Guardian.go
+++ Guardian.go
@@ -1,178 +1,74 @@
 package main
 
 import (
-	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"math"
 	"os"
 	"strconv"
-	"strings"
-)
 
-var db *DB
+	"github.com/plandem/xlsx"
+)
 
 type Record struct {
 	Position    string
-	Price       int
+	Quantity    int
+	Time        float64
 	Description string
 }
 
-type DB struct {
-	Records map[int]Record `json:"records"`
-}
-
-func makeDB() *DB {
-	db := DB{
-		make(map[int]Record),
-	}
-	return &db
-}
-
-func readDbFile() []byte {
-	dbFile, err := os.OpenFile("db.json", os.O_CREATE|os.O_RDWR, 0666)
+func main() {
+	xlsxFile := xlsx.New()
+	xlsxFile, err := xlsx.Open("data.xlsx")
 	if err != nil {
-		log.Fatal("problems with db.json file: ", err)
+		log.Fatal("Cant open *.xlsx file ", err)
 	}
-	defer dbFile.Close()
-	buffer, err := io.ReadAll(dbFile)
-	if err != nil {
-		log.Fatal("error reading file", err)
-	}
-	return buffer
-}
-
-func getRawData() {
-	positionFile, err := os.Open("Positions.txt")
-	if err != nil {
-		log.Fatal("error opening file", err)
-	}
-	defer positionFile.Close()
-	positions, err := io.ReadAll(positionFile)
-	if err != nil {
-		log.Fatal("error reading file", err)
-	}
-	priceFile, err := os.Open("Prices.txt")
-	if err != nil {
-		log.Fatal("error opening file", err)
-	}
-	defer priceFile.Close()
-	prices, err := io.ReadAll(priceFile)
-	if err != nil {
-		log.Fatal("error reading file", err)
-	}
-	descriptionFile, err := os.Open("Description.txt")
-	if err != nil {
-		log.Fatal("error opening file", err)
-	}
-	defer positionFile.Close()
-	descriptions, err := io.ReadAll(descriptionFile)
-	if err != nil {
-		log.Fatal("error reading file", err)
-	}
-	indexFile, err := os.Create("Index.csv")
-	if err != nil {
-		log.Fatal("cant create index file", err)
-	}
-	defer indexFile.Close()
-	w := csv.NewWriter(indexFile)
-	positionData := strings.Split(string(positions), "\n")
-	priceData := strings.Split(string(prices), "\n")
-	descriptionData := strings.Split(string(descriptions), "\n")
-	for idx := range positionData {
-		currentPrice := 0
-		positionData[idx] = strings.TrimSuffix(positionData[idx], "\r")
-		priceData[idx] = strings.TrimSuffix(priceData[idx], "\r")
-		descriptionData[idx] = strings.TrimSuffix(descriptionData[idx], "\r")
-		if positionData[idx] != "" {
-			currentPrice, err = strconv.Atoi(priceData[idx])
+	defer xlsxFile.Close()
+	sheet := xlsxFile.Sheet(0)
+	positions := sheet.Col(0).Values()
+	prices := sheet.Col(2).Values()
+	quantity := sheet.Col(3).Values()
+	time := sheet.Col(4).Values()
+	description := sheet.Col(5).Values()
+	positionsInOrder := make([]Record, 0)
+	orderTime := 0
+	for idx := range positions {
+		positionTime, err := strconv.Atoi(time[idx])
+		if err != nil || positionTime == 0 || prices[idx] == "" {
+			continue
+		}
+		if quantity[idx] != "" {
+			positionQuantity, err := strconv.Atoi(quantity[idx])
 			if err != nil {
 				log.Fatal(err)
 			}
-		} else {
-			continue
-		}
-		w.Write([]string{strconv.Itoa(idx + 1), positionData[idx]})
-		db.Records[idx+1] = Record{
-			positionData[idx],
-			currentPrice,
-			descriptionData[idx],
+			positionsInOrder = append(positionsInOrder, Record{positions[idx], positionQuantity, float64(positionTime), description[idx]})
+			orderTime += positionTime
 		}
 	}
-	w.Flush()
-	saveDB(db)
-
-}
-
-func init() {
-	db = makeDB()
-	buffer := readDbFile()
-	if len(buffer) == 0 {
-		getRawData()
-	} else {
-		err := json.Unmarshal(buffer, db)
-		if err != nil {
-			log.Fatal("db unmarshall error ", err)
-		}
-	}
-}
-
-func saveDB(db *DB) {
-	data, err := json.MarshalIndent(db, "  ", "   ")
-	if err != nil {
-		log.Println("marshalling error", err)
-	}
-	dbFile, err := os.OpenFile("db.json", os.O_RDWR, 0666)
-	if err != nil {
-		log.Fatal("problems with db.json file: ", err)
-	}
-	defer dbFile.Close()
-	_, err = io.WriteString(dbFile, string(data))
-	if err != nil {
-		log.Fatal("problems with saving to db.json file: ", err)
-	}
-
-}
-
-func main() {
 	outFile, err := os.Create("out.txt")
 	if err != nil {
-		log.Fatal("error creating out.txt file", err)
+		log.Fatal(err)
 	}
 	defer outFile.Close()
-	number := 1
-	positionNumber, quantity := 0, 0
 	outString := ""
-	orderTime := 0
-	orderPrice := 0
-	for {
-		fmt.Print("Номер позиции (0 для выхода): ")
-		fmt.Scan(&positionNumber)
-		if positionNumber == 0 {
-			orderDays := math.Round(float64(orderTime)/60./8.*10.) / 10.
-			finalString := fmt.Sprintf("ИТОГО:\nУдалённо - %d руб.  с учётом НДС 20%%\nВыезд - %.0f руб. с учётом НДС 20%%. Без учёта трансфера и проживания %0.1f дня.\nПри изменении технического задания, сумма ПНР может быть изменена в большую или меньшую сторону.\n", orderPrice, float64(orderPrice)*1.1, orderDays)
-			outFileWrite(outFile, finalString)
-			break
-		}
-		fmt.Print("Количество: ")
-		fmt.Scan(&quantity)
-		record, ok := db.Records[positionNumber]
-		if !ok {
-			fmt.Println("Нет такой позиции")
-			continue
-		}
-		workHours, positionPrice := orderPositionAttributes(record, quantity)
-		outString = fmt.Sprintf("%d. %s\n%s\n", number, record.Position, record.Description)
-		outString += fmt.Sprintf("%d шт., %0.1f ч., %d руб. с учётом НДС 20%% \n\n", quantity, workHours, positionPrice)
-		outFileWrite(outFile, outString)
-
-		outString = ""
-		number += 1
-		orderTime += record.Price * quantity
-		orderPrice += positionPrice
+	for idx, position := range positionsInOrder {
+		positionHours, positionPrice := positionAttributes(position)
+		outString = fmt.Sprintf("%d. %s\n%s\n", idx+1, position.Position, position.Description)
+		outString += fmt.Sprintf("%d шт., %0.1f ч., %d руб. с учётом НДС 20%% \n\n", position.Quantity, positionHours, positionPrice)
 	}
+
+	// 	workHours, positionPrice := orderPositionAttributes(record, quantity)
+	//
+	// 	outString += fmt.Sprintf("%d шт., %0.1f ч., %d руб. с учётом НДС 20%% \n\n", quantity, workHours, positionPrice)
+	// 	outFileWrite(outFile, outString)
+
+	// 	outString = ""
+	// 	number += 1
+	// 	orderTime += record.Price * quantity
+	// 	orderPrice += positionPrice
+	//
 }
 
 func outFileWrite(outFile *os.File, outString string) {
@@ -182,8 +78,8 @@ func outFileWrite(outFile *os.File, outString string) {
 	}
 }
 
-func orderPositionAttributes(record Record, quantity int) (float64, int) {
-	workHours := math.Round(float64(record.Price*quantity)/60.*10.) / 10
-	positionPrice := int(float64(record.Price*quantity*60) * 1.2)
-	return workHours, positionPrice
+func positionAttributes(position Record) (float64, int) {
+	positionHours := math.Round(float64(position.Time)/60.*10.) / 10
+	positionPrice := int(position.Time * float64(position.Quantity) * 60. * 1.2)
+	return positionHours, positionPrice
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module Guardian.go
 
 go 1.19
+
+require github.com/plandem/xlsx v1.0.4
+
+require github.com/plandem/ooxml v1.1.2 // indirect
